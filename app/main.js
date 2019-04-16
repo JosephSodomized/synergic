c36f9f890739e560d75b90b640ae9f6ba8839c07
Dominik Malcharczyk <malcharczyk.dominik@gmail.com>
2019-04-17 00:33:35 +0200

    first commit

---
+++ app/main.js
@@ -0,0 +1,4 @@
+$('#text1').addClass('animated').fadeIn(1200).fadeOut(1200);
+$('#text2').addClass('animated').delay(2200).fadeIn(1200).fadeOut(1200);
+$('#text3').addClass('animated').delay(4200).fadeIn(1200);
+$('.sidebar').addClass('animated').delay(4300).fadeIn(2000).animate({ right: 0 }, 1000);
